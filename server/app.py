24a72d02533e29d7eeb6d31403a73c3afb8bca52
Priscilla-22 <priscillamw123@gmail.com>
2024-04-13 10:30:08 +0300

    feat:Cookies and Sessions

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -15,20 +15,38 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/clear')
 def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
+
 @app.route('/articles')
 def index_articles():
-
+    articles = [article.to_dict() for article in Article.query.all()]
+    response = make_response(
+        jsonify(articles),
+        200
+    )
+    return response
     pass
+
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    session['page_views'] = session.get('page_views', 0) + 1
+
+    if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first()
+        article_data = article.to_dict()
+        return jsonify(article_data), 200
+    else:
+        error_message = {'message': 'Maximum pageview limit reached'}
+        return jsonify(error_message), 401
 
     pass
 
-if __name__ == '__main__':
+
+if __name__ == '_main_':
     app.run(port=5555)
